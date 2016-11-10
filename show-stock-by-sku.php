7f5f0eae7d761b4f19c6e5a912353f64db80aef6
Alvaro Zweig <alvaro@ieholding.com>
2016-11-10 13:10:09 -0500

    Update show-stock-by-sku.php
    I removed the bootstrap call

--- show-stock-by-sku.php
+++ show-stock-by-sku.php
@@ -23,43 +23,36 @@
  * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 // Change current directory to the directory of current script
 chdir(dirname(__FILE__));
 
-require 'app/bootstrap.php';
 require 'app/Mage.php';
-
 if (!Mage::isInstalled()) {
-	echo "Application is not installed yet, please complete install wizard first.";
+  echo "Application is not installed yet, please complete install wizard first.";
     exit;
 }
-
 // Only for urls
 // Don't remove this
 $_SERVER['SCRIPT_NAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_NAME']);
 $_SERVER['SCRIPT_FILENAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_FILENAME']);
 
+
 Mage::app('admin')->setUseSessionInUrl(false);
-
 umask(0);
-
 try {
     $store = Mage::app()->getStore();
     $name = $store->getName();
-
-	$_sku = $_REQUEST['sku'];
-	if(empty($_sku)){
-		echo "SKU not found.";
-		exit(1);
-	}
-	$_catalog = Mage::getModel('catalog/product');
-	$_productId = $_catalog->getIdBySku($_sku);
-	$_product = Mage::getModel('catalog/product')->load($_productId);
-	$qty = Mage::getModel('cataloginventory/stock_item')->loadByProduct($_product)->getQty();
-	header('Content-Type: application/json');
-	echo '{"SKU":"'.$_sku.'","TotalQuantity":'.intval($qty).',"StockByWarehouse":[{"WarehouseIdentifier":1,"Name":"'.$name.'","Quantity":'.intval($qty).'}]}';
-
+  $_sku = $_REQUEST['sku'];
+  if(empty($_sku)){
+    echo "SKU not found.";
+    exit(1);
+  }
+  $_catalog = Mage::getModel('catalog/product');
+  $_productId = $_catalog->getIdBySku($_sku);
+  $_product = Mage::getModel('catalog/product')->load($_productId);
+  $qty = Mage::getModel('cataloginventory/stock_item')->loadByProduct($_product)->getQty();
+  header('Content-Type: application/json');
+  echo '{"SKU":"'.$_sku.'","TotalQuantity":'.intval($qty).',"StockByWarehouse":[{"WarehouseIdentifier":1,"Name":"'.$name.'","Quantity":'.intval($qty).'}]}';
 } catch (Exception $e) {
     Mage::printException($e);
     exit(1);
